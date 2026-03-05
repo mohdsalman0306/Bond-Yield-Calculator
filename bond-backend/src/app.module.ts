4086ec52ab0637959353b8d54db7a654ade562eb
Salman <mohd.salman0306@gmail.com>
2026-03-05 22:28:52 +0400

    Fix cors error on production

--- bond-backend/src/app.module.ts
+++ bond-backend/src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BondModule } from './bond/bond.module';
 
 @Module({
-  imports: [BondModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    BondModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- bond-backend/src/main.ts
+++ bond-backend/src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Read allowed frontends from environment
+  const allowedOrigins = process.env.ALLOWED_FRONTENDS
+    ? process.env.ALLOWED_FRONTENDS.split(',')
+    : [];
+
+  // Enable CORS for frontend
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error(`CORS not allowed for ${origin}`));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  });
+
   app.useGlobalPipes(new ValidationPipe(
     {
       whitelist: true,
@@ -11,11 +29,6 @@ async function bootstrap() {
       transform: true,
     }
   ));
-  // Enable CORS for frontend
-  app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
